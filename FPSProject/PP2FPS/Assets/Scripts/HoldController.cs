265a4e9f247914d45242e8ae540f276ebe0cdf63
Scott Wollbrink <sawollbrink@student.fullsail.edu>
2024-05-12 14:45:36 -0500

    Created a secondary camera that would only show held obj and created a spike trap prefab

--- FPSProject/PP2FPS/Assets/Scripts/HoldController.cs
+++ FPSProject/PP2FPS/Assets/Scripts/HoldController.cs
@@ -12,40 +12,52 @@ public class HoldController : MonoBehaviour
     [SerializeField] float launchStrength;
 
 
-    [HideInInspector] public GameObject hold;
+    [HideInInspector] public GameObject heldObj;
     [SerializeField] Rigidbody rb;
 
     [HideInInspector] public bool hasCube;
 
+    private int layerToShow;
+
+    private void Start()
+    {
+        layerToShow = LayerMask.NameToLayer("HeldObj");
+    }
     private void Update()
     {
         if (Input.GetButtonDown("Pickup"))
         {
-            if (hold == null)
+            if (heldObj == null)
             {
                 pickUp();
             }
             else
             {
-                winCube cube = hold.GetComponent<winCube>();
+                winCube cube = heldObj.GetComponent<winCube>();
+                doNotDropThroughObj();
                 drop();
                 cube.ApplyVelocity(GameManager.Instance.playerCntrl.GetVelocity());
             }
         }
-        if (hold != null)
+        if (heldObj != null)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                winCube cube = hold.GetComponent<winCube>();
+                winCube cube = heldObj.GetComponent<winCube>();
+                doNotDropThroughObj();
                 drop();
                 cube.ApplyVelocity((Camera.main.transform.forward * launchStrength) + GameManager.Instance.playerCntrl.GetVelocity());
             }
             //if the pickup object is not close enough to the player move the object to the hold location
-            else if(Vector3.Distance(hold.transform.position, holdPos.position) > 0.1f)
+            else if(Vector3.Distance(heldObj.transform.position, holdPos.position) > 0.1f)
             {
-                Vector3 moveDirection = holdPos.position - hold.transform.position;
+                Vector3 moveDirection = holdPos.position - heldObj.transform.position;
                 rb.AddForce(moveDirection * pickupSpeed);
             }
+        }
+        if (hasCube && Vector3.Distance(rb.position, holdPos.position) < 0.1f)
+        {
+            rb.isKinematic = true;
         }
     }
 
@@ -59,13 +71,16 @@ public class HoldController : MonoBehaviour
             if (hit.transform.gameObject.GetComponent<Rigidbody>())
             {
                 hasCube = true;
+                heldObj = hit.transform.gameObject;
+                heldObj.layer = layerToShow;
+                heldObj.GetComponent<Collider>().enabled = false;
                 rb = hit.transform.gameObject.GetComponent<Rigidbody>();
                 rb.useGravity = false;
                 rb.drag = 8;
                 rb.constraints = RigidbodyConstraints.FreezeRotation;
 
                 rb.transform.parent = holdPos;
-                hold = hit.transform.gameObject;
+                heldObj = hit.transform.gameObject;
 
                 GameManager.Instance.playerCntrl.runSpeed -= GameManager.Instance.playerCntrl.holdSpeed;
             }
@@ -75,17 +90,31 @@ public class HoldController : MonoBehaviour
     public void drop()
     {
         hasCube = false;
-        if (hold != null)
+        if (heldObj != null)
         {
             //drop
+            heldObj.layer = 0;
+            heldObj.GetComponent<Collider>().enabled = true;
             rb.useGravity = true;
             rb.drag = .3f;
             rb.constraints = RigidbodyConstraints.None;
-
-            hold.transform.parent = null;
-            hold = null;
+            rb.isKinematic = false;
+            heldObj.transform.parent = null;
+            heldObj = null;
 
             GameManager.Instance.playerCntrl.runSpeed += GameManager.Instance.playerCntrl.holdSpeed;
+        }
+    }
+    void doNotDropThroughObj()
+    {
+        float rangeToCube = Vector3.Distance(heldObj.transform.position, transform.position) + 1;
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), rangeToCube);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.red, rangeToCube);
+        if (hits.Length > 0) 
+        {
+            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f);
         }
     }
 }
