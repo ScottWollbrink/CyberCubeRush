befbd5ffb24ac1d757cac4c9f3bf98d4a8daaa9d
Revan-Saber <cwmcadow@gmail.com>
2024-05-13 18:38:37 -0500

    Level 5 start and looping platform
    I've made the basic starts of level 5, namely the floor filled with a poison trap and a starting area with an elevator from the poison to the top of the starting area. I have also made a platform that moves through an array of positions. I also started to add some mathmatics so the looping platform would move in an actual circular motion, but I have commented these mechanics out until such time that we decide to use them.

---
+++ FPSProject/CyberCubeRush/Assets/Scripts/LoopingPlatform.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class LoopingPlatform : MonoBehaviour
+{
+    [SerializeField] Transform[] points;
+    [SerializeField] Transform platform;
+    [SerializeField] float speed;
+    int nextPoint;
+    [Header("Circular moving")]
+    [SerializeField] Transform center;
+    [SerializeField] float rotationRadius;
+    float posX, posY, posZ, angle = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//circular looping mechanics to be implemented later
+        //posX = center.position.x + Mathf.Cos(angle) * rotationRadius;
+        //posY = center.position.y + Mathf.Sin(angle) * rotationRadius;
+        //posZ = center.position.z + Mathf.Tan(angle) * rotationRadius;
+        Vector3 targetLocation = points[nextPoint].position;
+
+        platform.position = Vector3.MoveTowards(platform.position, targetLocation, speed * Time.deltaTime);
+        //angle = angle + Time.deltaTime * speed;
+//        if (angle >= 360)
+ //           angle -= 360;
+        if ((targetLocation - platform.position).magnitude <= 0.3f)
+        {
+            nextPoint++;
+            if(nextPoint >= points.Length)
+            {
+                nextPoint = 0;
+            }
+        }
+    }
+
+    
+}
