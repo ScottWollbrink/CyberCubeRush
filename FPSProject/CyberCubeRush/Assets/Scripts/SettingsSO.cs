dd15a4d854e5eb13dca858c8bad1dafaa618055a
Benjamin Bissett <91335118+benjaminbiss@users.noreply.github.com>
2024-05-14 10:04:48 -0600

    Created a Scene File structure and a script that rotates a world space ui to face the player

--- FPSProject/CyberCubeRush/Assets/Scripts/SettingsSO.cs
+++ FPSProject/CyberCubeRush/Assets/Scripts/SettingsSO.cs
@@ -7,7 +7,7 @@ public class SettingsSO : ScriptableObject
 {
     public bool showReticle = true;
     public bool invertMouse = false;
-    public float mouseSense = 100f;
-    public float musicVolume = 50f;
-    public float sfxVolume = 50f;
+    public float mouseSense = .25f;
+    public float musicVolume = .25f;
+    public float sfxVolume = .5f;
 }
